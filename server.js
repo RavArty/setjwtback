5995c88d8d8f9636359c6b8eac095a2357071c45
Ravshan <ravkart@gmail.com>
2019-09-16 14:19:37 -0700

    init commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json()); 
+
+app.get('/', (req, res) => {
+  res.send('hello')
+})
+app.listen(3000, ()=> {
+  console.log('app is running on port 3000');
+})
